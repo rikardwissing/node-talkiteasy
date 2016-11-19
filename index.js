2db636b5cc87b7f0e30241d04839eb449d585b2a
rikardwissing <rikard@nocweb.se>
2016-11-20 00:11:28 +0100

    fix

--- index.js
+++ index.js
@@ -43,7 +43,8 @@ module.exports = function (app) {
   if(app) {
     app.get('/say/:text.:codec', (req, res) => module.pipe(res, req.params.text, reqToOptions(req)));
     app.get('/say/:text', (req, res) => module.pipe(res, req.params.text, reqToOptions(req)));
-    app.get('/tts/voice.mp3', (req, res) => module.pipe(res, req.query.text, reqToOptions(req)));
+    app.get('/say/:text/voice.:codec', (req, res) => module.pipe(res, req.params.text, reqToOptions(req)));
+    app.get('/tts/voice.:codec', (req, res) => module.pipe(res, req.query.text, reqToOptions(req)));
   }
 
   return module;

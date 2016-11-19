aace227c4d37d7058d7685868d4360b1d54d64c4
rikardwissing <rikard@nocweb.se>
2016-11-19 03:28:09 +0100

    made app optional

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 var request = require('request');
 var querystring = require('querystring');
 
+var defaultOptions = function(options) {
+  options = options || {};
+
+  options.service = options.service || "google";
+  options.language = options.language || "sv-se";
+  options.token = options.token || 'b358d0103d7642269a19753d527023e6';
+  options.codec = options.codec || "mp3";
+  options.format = options.format || "48khz_16bit_stereo";
+  options.rate = options.rate || "0";
+
+  return options;
+}
+
 var getQueryObject = function(text, options) {
   if(options.service == 'google') {
     return {
@@ -23,51 +36,44 @@ var getQueryObject = function(text, options) {
   }
 }
 
-var defaultOptions = function(options) {
-  options = options || {};
+var getUrl = function(text, options) {
+  options = defaultOptions(options);
 
-  options.service = options.service || "google";
-  options.language = options.language || "sv-se";
-  options.token = options.token || 'b358d0103d7642269a19753d527023e6';
-  options.codec = options.codec || "mp3";
-  options.format = options.format || "48khz_16bit_stereo";
-  options.rate = options.rate || "0";
+  var qobject = getQueryObject(text, options);
+  var qstring = querystring.stringify(qobject);
 
-  return options;
+  if(options.service == 'google') {
+    return 'http://translate.google.com/translate_tts?'+qstring;
+  }
+
+  if(options.service == 'voicerss') {
+    return 'http://api.voicerss.org/?'+qstring;
+  }
 }
 
 module.exports = function (app) {
   var module = {};
 
-  app.get('/tts/:service/:text.:codec', function(req, res) {
-    module.pipe(res, params.text, {
-      service: req.params.service,
-      codec: req.params.codec,
-      format: req.query.f,
-      rate: req.query.r,
-      language: req.query.l
-    });
-  });
-
   module.getUrl = function(text, options) {
-    options = defaultOptions(options);
-
-    var qobject = getQueryObject(text, options);
-    var qstring = querystring.stringify(qobject);
-
-    if(options.service == 'google') {
-      return 'http://translate.google.com/translate_tts?'+qstring;
-    }
-
-    if(options.service == 'voicerss') {
-      return 'http://api.voicerss.org/?'+qstring;
-    }
+    return getUrl(text, options);
   };
 
   module.pipe = function(res, text, options) {
     options = defaultOptions(options);
     request(module.getUrl(text, options)).pipe(res);
   };
+
+  if(app) {
+    app.get('/tts/:service/:text.:codec', function(req, res) {
+      module.pipe(res, params.text, {
+        service: req.params.service,
+        codec: req.params.codec,
+        format: req.query.f,
+        rate: req.query.r,
+        language: req.query.l
+      });
+    });
+  }
 
   return module;
 }

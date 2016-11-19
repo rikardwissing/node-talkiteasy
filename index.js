bdfdd9beed43a4293de45892d9f4f68fb51cad65
rikardwissing <rikard@nocweb.se>
2016-11-19 16:18:40 +0100

    Cleanup

--- index.js
+++ index.js
@@ -1,90 +1,62 @@
-var request = require('request');
-var querystring = require('querystring');
+"use strict";
 
-var defaultOptions = function(options) {
-  options = options || {};
+const request = require('request');
+const querystring = require('querystring');
 
-  options.service = options.service || "google";
-  options.language = options.language || "sv-se";
-  options.token = options.token || 'b358d0103d7642269a19753d527023e6';
-  options.codec = options.codec || "mp3";
-  options.format = options.format || "48khz_16bit_stereo";
-  options.rate = options.rate || "0";
+let defaultOptions = (options) => {
+  options = options || {};
 
-  return options;
-}
-
-var getQueryObject = function(text, options) {
-  if(options.service == 'google') {
-    return {
-      q: text,
-      tl: options.language,
-      ie: 'UTF-8',
-      client: 'tw-ob'
-    };
-  }
-
-  if(options.service == 'voicerss') {
-    return {
-      key: options.token,
-      src: text, 
-      hl: options.language,
-      f: options.format,
-      r: options.rate,
-      c: options.codec
-    };
-  }
-}
-
-var getUrl = function(text, options) {
-  options = defaultOptions(options);
-
-  var qobject = getQueryObject(text, options);
-  var qstring = querystring.stringify(qobject);
-
-  if(options.service == 'google') {
-    return 'http://translate.google.com/translate_tts?'+qstring;
-  }
-
-  if(options.service == 'voicerss') {
-    return 'http://api.voicerss.org/?'+qstring;
-  }
-}
-
-var reqToOptions = function(req){
   return {
-    service: req.params.service || req.query.s,
-    codec: req.params.codec || req.query.c,
-    format: req.query.f,
-    rate: req.query.r,
-    language: req.query.l
+    service: options.service || 'google',
+    language: options.language || 'en-us',
+    token: options.token || 'b358d0103d7642269a19753d527023e6',
+    codec: options.codec || 'mp3',
+    format: options.format || '48khz_16bit_stereo',
+    rate: options.rate || '0'
   }
 }
+
+let reqToOptions = (req) => ({
+  service: req.params.service || req.query.s,
+  codec: req.params.codec || req.query.c,
+  format: req.query.f,
+  rate: req.query.r,
+  language: req.query.l
+});
+
+let getUrl = (text, options) => options.service == 'google' ? getUrlGoogle(text, options)
+                              : options.service == 'voicerss' ? getUrlVoiceRSS(text, options)
+                              : '';
+
+let getUrlGoogle = (text, options) => 'http://translate.google.com/translate_tts?'+querystring.stringify(getQueryObjectGoogle(text, options));
+let getUrlVoiceRSS = (text, options) => 'http://api.voicerss.org/?'+querystring.stringify(getQueryObjectVoiceRSS(text, options));
+
+let getQueryObjectGoogle = (text, options) => ({
+  q: text,
+  tl: options.language,
+  ie: 'UTF-8',
+  client: 'tw-ob'
+});
+
+let getQueryObjectVoiceRSS = (text, options) => ({
+  key: options.token,
+  src: text, 
+  hl: options.language,
+  f: options.format,
+  r: options.rate,
+  c: options.codec
+});
 
 module.exports = function (app) {
-  var module = {};
+  let module = {};
 
-  module.getUrl = function(text, options) {
-    return getUrl(text, options);
-  };
-
-  module.pipe = function(res, text, options) {
-    options = defaultOptions(options);
-    request(module.getUrl(text, options)).pipe(res);
-  };
+  module.getUrl = (text, options) => getUrl(text, defaultOptions(options));
+  module.pipe = (res, text, options) => request(module.getUrl(text, defaultOptions(options))).pipe(res);
 
   if(app) {
-    app.get('/say/:text.:codec', function(req, res) {
-      module.pipe(res, req.params.text, reqToOptions(req));
-    });
-
-    app.get('/say/:text', function(req, res) {
-      module.pipe(res, req.params.text, reqToOptions(req));
-    });
-
-    app.get('/tts/:service/:text.:codec', function(req, res) {
-      module.pipe(res, req.params.text, reqToOptions(req));
-    });
+    app.get('/say/:text.:codec', (req, res) => module.pipe(res, req.params.text, reqToOptions(req)));
+    app.get('/say/:text', (req, res) => module.pipe(res, req.params.text, reqToOptions(req)));
+    app.get('/tts/:service/:text.:codec', (req, res) => module.pipe(res, req.params.text, reqToOptions(req)));
   }
 
   return module;
